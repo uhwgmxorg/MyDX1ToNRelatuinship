065ff349914cd49b104c6813f2b6f6766d0c97cc
Uwe Walter <uhw@gmx.org>
2018-10-10 15:41:31 +0200

    Start.

---
+++ MyDX1ToNRelatuinship/FormMain.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace MyDX1ToNRelatuinship
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraGrid.GridLevelNode gridLevelNode1 = new DevExpress.XtraGrid.GridLevelNode();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.testDBDataSet1 = new MyDX1ToNRelatuinship.TestDBDataSet();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colFirstName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colLastName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colInsertDate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.personTableAdapter1 = new MyDX1ToNRelatuinship.TestDBDataSetTableAdapters.PersonTableAdapter();
+            this.bookTableAdapter1 = new MyDX1ToNRelatuinship.TestDBDataSetTableAdapters.BookTableAdapter();
+            this.buttonReload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDBDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridView2
+            // 
+            this.gridView2.DetailHeight = 284;
+            this.gridView2.GridControl = this.gridControl1;
+            this.gridView2.Name = "gridView2";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridControl1.DataMember = "Person";
+            this.gridControl1.DataSource = this.testDBDataSet1;
+            // 
+            // 
+            // 
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(2);
+            gridLevelNode1.LevelTemplate = this.gridView2;
+            gridLevelNode1.RelationName = "FK__Book__InsertDate__3B75D760";
+            this.gridControl1.LevelTree.Nodes.AddRange(new DevExpress.XtraGrid.GridLevelNode[] {
+            gridLevelNode1});
+            this.gridControl1.Location = new System.Drawing.Point(9, 10);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(514, 346);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1,
+            this.gridView2});
+            // 
+            // testDBDataSet1
+            // 
+            this.testDBDataSet1.DataSetName = "TestDBDataSet";
+            this.testDBDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colId,
+            this.colFirstName,
+            this.colLastName,
+            this.colInsertDate});
+            this.gridView1.DetailHeight = 284;
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colId
+            // 
+            this.colId.FieldName = "Id";
+            this.colId.MinWidth = 19;
+            this.colId.Name = "colId";
+            this.colId.Visible = true;
+            this.colId.VisibleIndex = 0;
+            this.colId.Width = 70;
+            // 
+            // colFirstName
+            // 
+            this.colFirstName.FieldName = "FirstName";
+            this.colFirstName.MinWidth = 19;
+            this.colFirstName.Name = "colFirstName";
+            this.colFirstName.Visible = true;
+            this.colFirstName.VisibleIndex = 1;
+            this.colFirstName.Width = 70;
+            // 
+            // colLastName
+            // 
+            this.colLastName.FieldName = "LastName";
+            this.colLastName.MinWidth = 19;
+            this.colLastName.Name = "colLastName";
+            this.colLastName.Visible = true;
+            this.colLastName.VisibleIndex = 2;
+            this.colLastName.Width = 70;
+            // 
+            // colInsertDate
+            // 
+            this.colInsertDate.FieldName = "InsertDate";
+            this.colInsertDate.MinWidth = 19;
+            this.colInsertDate.Name = "colInsertDate";
+            this.colInsertDate.Visible = true;
+            this.colInsertDate.VisibleIndex = 3;
+            this.colInsertDate.Width = 70;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.Location = new System.Drawing.Point(535, 334);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(56, 22);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // personTableAdapter1
+            // 
+            this.personTableAdapter1.ClearBeforeFill = true;
+            // 
+            // bookTableAdapter1
+            // 
+            this.bookTableAdapter1.ClearBeforeFill = true;
+            // 
+            // buttonReload
+            // 
+            this.buttonReload.Location = new System.Drawing.Point(535, 306);
+            this.buttonReload.Name = "buttonReload";
+            this.buttonReload.Size = new System.Drawing.Size(56, 23);
+            this.buttonReload.TabIndex = 3;
+            this.buttonReload.Text = "Reload";
+            this.buttonReload.UseVisualStyleBackColor = true;
+            this.buttonReload.Click += new System.EventHandler(this.buttonReload_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.buttonReload);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.buttonClose);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormMain";
+            this.Text = "MyDX1ToNRelatuinship";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDBDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private TestDBDataSet testDBDataSet1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colId;
+        private DevExpress.XtraGrid.Columns.GridColumn colFirstName;
+        private DevExpress.XtraGrid.Columns.GridColumn colLastName;
+        private DevExpress.XtraGrid.Columns.GridColumn colInsertDate;
+        private TestDBDataSetTableAdapters.PersonTableAdapter personTableAdapter1;
+        private TestDBDataSetTableAdapters.BookTableAdapter bookTableAdapter1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private System.Windows.Forms.Button buttonReload;
+    }
+}
+
---
+++ MyDX1ToNRelatuinship/FormMain.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyDX1ToNRelatuinship
+{
+    public partial class FormMain : Form
+    {
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FormMain()
+        {
+            InitializeComponent();
+            gridView1.CellValueChanged += GridView1_CellValueChanged;
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        /// <summary>
+        /// buttonReload_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonReload_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        /// <summary>
+        /// buttonClose_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            SaveData();
+            Close();
+        }
+
+        #endregion
+
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        /// <summary>
+        /// FormMain_Load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+
+        /// <summary>
+        /// GridView1_CellValueChanged
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GridView1_CellValueChanged(object sender, DevExpress.XtraGrid.Views.Base.CellValueChangedEventArgs e)
+        {
+            SaveData();
+        }
+
+        #endregion
+
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// LoadDate
+        /// </summary>
+        private void LoadData()
+        {
+            personTableAdapter1.Fill(testDBDataSet1.Person);
+            bookTableAdapter1.Fill(testDBDataSet1.Book);
+        }
+
+        /// <summary>
+        /// SaveDate
+        /// </summary>
+        private void SaveData()
+        {
+            personTableAdapter1.Update(testDBDataSet1.Person);
+            bookTableAdapter1.Update(testDBDataSet1.Book);
+        }
+
+        #endregion
+    }
+}
